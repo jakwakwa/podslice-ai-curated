74f311ad95eaafd05bf38bb06e0a295e0963e490
Jaco Kotzee <jakwakwa@gmail.com>
2025-10-11 19:07:26 +0200

    style: update Sheet component for improved accessibility and aesthetics
    - Enhanced the SheetPrimitive.Close button with a more visually appealing design, including updated colors and sizes for better visibility.
    - Improved accessibility by changing the close button label to "Close Player" for clearer context.

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -60,9 +60,9 @@ function SheetContent({
 				{/* Visually hidden title for a11y compliance */}
 				<SheetPrimitive.Title className="sr-only">{ariaLabel}</SheetPrimitive.Title>
 				{children}
-				<SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-background absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-					<XIcon className="size-4" />
-					<span className="sr-only">Close</span>
+				<SheetPrimitive.Close className="flex flex-col items-center gap-2 ring-offset-background focus:ring-ring data-[state=open]:bg-[#fff] absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none text-[0.55rem] text-purple-400">
+					<XIcon color="#B06CF0" className="size-6 border-1 border-[#7528cd] rounded-full p-1" />
+					<span className="">Close Player</span>
 				</SheetPrimitive.Close>
 			</SheetPrimitive.Content>
 		</SheetPortal>
