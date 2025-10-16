9220901d0afcb0d221054c664a46d4e9b160cb5d
Jaco Kotzee <jakwakwa@gmail.com>
2025-10-16 19:32:07 +0200

    style: update global styles and footer component for improved UX
    - Modified color variables in globals.css to enhance visual consistency, including updates to primary and secondary colors, link hover states, and animated gradients.
    - Adjusted footer component padding for better spacing and alignment.
    - Improved layout and styling in the RecentEpisodesList component for a more cohesive appearance.

--- app/(protected)/footer.tsx
+++ app/(protected)/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function Footer() {
 	return (
-		<div className=" px-4 py-8 mb-0 w-screen md:-ml-4 bottom-0 md:w-[102%] flex flex-col gap-4 md:py-12 items-center justify-center text-xs overflow-hidden rounded-none md:bg-bigcard opacity-90 ">
+		<div className=" px-4 py-14 mb-0 w-screen md:-ml-4 bottom-0 md:w-[102%] flex flex-col gap-4 md:py-24 items-center justify-center text-xs overflow-hidden rounded-none md:bg-bigcard opacity-90 ">
 			<div className="flex items-center justify-center gap-2 text-sm text-secondary  w-full text-center" >
 				<Link href="/terms" className="hover:text-secondary-foreground transition-colors">Terms of use</Link>
 				<span>|</span>
