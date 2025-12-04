f0153d2438a83fab2a4ee99fc8ceaf17fedaf15c
Jaco Kotzee <jakwakwa@gmail.com>
2025-12-04 13:58:33 +0200

    feat: introduce new landing page components including header, hero carousel, and various content sections.

---
+++ components/alternate-landingpage/alt-page.tsx
@@ -0,0 +1,11 @@
+import { HeroCarousel } from "@/components/hero-carousel"
+import { Header } from "@/components/header"
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Header />
+      <HeroCarousel />
+    </main>
+  )
+}
